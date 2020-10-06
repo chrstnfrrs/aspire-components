99df9ecb02cbd0e8f0542816c86384689793ee7b
Christian Farris <chrstnfrrs@Christians-MacBook-Pro.local>
2020-10-05 22:01:56 -0500

    initial project setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <a-container justify="start">
+    <h1>Aspire Components</h1>
+    <a-btn>asdf</a-btn>
+  </a-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ABtn from './components/ABtn.vue'
+import AContainer from './components/AContainer.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ABtn,
+    AContainer
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.a-container {
+  height: 100vh;
+  width: 100vhh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
