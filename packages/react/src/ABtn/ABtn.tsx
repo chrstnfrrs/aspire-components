4f090669baa8e2e8adb0d20e09f5ccb04d9d9590
Christian Farris <christianfarriscs@gmail.com>
2021-01-25 22:51:52 -0600

    add classList to create classes for a-btn

--- packages/react/src/ABtn/ABtn.tsx
+++ packages/react/src/ABtn/ABtn.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-import { ABtnProps } from './ABtn.types'
+import {isJustifiable, isAlignable} from 'aspire-components-helpers'
+import 'aspire-components-scss/build/index.scss'
 
+import { ABtnProps } from './ABtn.types'
 import './ABtn.scss'
 
-const ABtn: React.FC<ABtnProps> = ({text}) => (
-  <button className={'a-btn'}>{text}</button>
-)
+const ABtn: React.FC<ABtnProps> = (props) => {
+  const classList = [
+    'a-btn',
+    ...isJustifiable(props.justify),
+    ...isAlignable(props.align),
+  ]
+
+  return <button className={classList.join(' ')}>{props.children}</button>
+}
 
 export default ABtn
