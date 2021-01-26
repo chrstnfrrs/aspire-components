54fe3a681c0b87c2917db9e5209594f78e67339d
Christian Farris <christianfarriscs@gmail.com>
2021-01-25 22:52:15 -0600

    create a-btn tests

--- packages/react/src/tests/ABtn.spec.tsx
+++
@@ -1,9 +0,0 @@
-import {render} from "@testing-library/react"
-
-import ABtn from '../ABtn/ABtn'
-
-describe('ABtn', () => {
-  test('should be true', () => {
-    expect(true).toBe(true)
-  });
-});
