99a6fd1b43a7545007d848cc0d23e03b2999d0a7
Christian Farris <christianfarriscs@gmail.com>
2021-01-26 19:05:12 -0600

    create list of modifiable functions and prop names

---
+++ packages/react/tests/enums/staticClasses.ts
@@ -0,0 +1,11 @@
+import { isAlignable, isJustifiable} from 'aspire-components-helpers'
+
+export const justifiable = {
+  method: isJustifiable,
+  field: 'justify'
+}
+
+export const alignable = {
+  method: isAlignable,
+  field: 'align'
+}
