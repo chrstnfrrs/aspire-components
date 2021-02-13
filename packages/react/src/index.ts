51f18b17f2e729b317ef5008b2df7854e4524808
Christian Farris <christianfarriscs@gmail.com>
2021-02-12 22:59:58 -0600

    export new components

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,9 +1,17 @@
 import ABtn from './ABtn/ABtn'
 import AContainer from './AContainer/AContainer'
+import AHeader from './AHeader/AHeader'
+import AHeading from './AHeading/AHeading'
+import AHero from './AHero/AHero'
 import ANav from './ANav/ANav'
+import ASection from './ASection/ASection'
 
 export { 
   ABtn,
   AContainer,
+  AHeader,
+  AHeading,
+  AHero,
   ANav,
+  ASection,
 }
