a3cc7cf8d26ed18b43e42cd2473f3587235a2f73
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 18:48:56 -0600

    Testing padding prop

--- packages/react/src/ANav/ANav.tsx
+++ packages/react/src/ANav/ANav.tsx
@@ -7,14 +7,34 @@ import 'aspire-components-scss/build/index.scss'
 import { ANavProps } from './ANav.types'
 import './ANav.scss'
 
+const createPadding = (padding) => {
+  const paddingMap = {
+    p4: 'p4'
+  }
+
+  return paddingMap[padding]
+}
+
 const ANav: React.FC<ANavProps> = (props) => {
+
   const classList = [
     'a-nav',
     isJustifiable(props.justify),
     isAlignable(props.align),
+    createPadding(props.padding)
   ]
 
-  return <nav className={clsx(props.className, classList)}>{props.children}</nav>
+  let styles = {}
+
+  if (!createPadding(props.padding) && props.padding) {
+    styles = {
+      ...styles,
+      padding: props.padding
+    }
+  } 
+
+
+  return <nav className={clsx(props.className, classList)} style={styles}>{props.children}</nav>
 }
 
 export default ANav
