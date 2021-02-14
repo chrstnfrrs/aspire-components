0ba733a6709dc8b85973a37c2b7d9ca57ba2148b
Christian Farris <christianfarriscs@gmail.com>
2021-02-13 18:48:29 -0600

    Implement as prop on AHeading

--- packages/react/src/AHeading/AHeading.tsx
+++ packages/react/src/AHeading/AHeading.tsx
@@ -7,6 +7,22 @@ import 'aspire-components-scss/build/index.scss'
 import { AHeadingProps } from './AHeading.types'
 import './AHeading.scss'
 
+const AH1 = (props) => <h1 className={props.className}>{props.children}</h1>
+const AH2 = (props) => <h2 className={props.className}>{props.children}</h2>
+const AH3 = (props) => <h3 className={props.className}>{props.children}</h3>
+const AH4 = (props) => <h4 className={props.className}>{props.children}</h4>
+const AH5 = (props) => <h5 className={props.className}>{props.children}</h5>
+const AH6 = (props) => <h6 className={props.className}>{props.children}</h6>
+
+const AHeaderTypeMap = {
+  h1: AH1,
+  h2: AH2,
+  h3: AH3,
+  h4: AH4,
+  h5: AH5,
+  h6: AH6,
+}
+
 const AHeading: React.FC<AHeadingProps> = (props) => {
   const classList = [
     'a-heading',
@@ -14,7 +30,9 @@ const AHeading: React.FC<AHeadingProps> = (props) => {
     isAlignable(props.align),
   ]
 
-  return <h1 className={clsx(props.className, classList)}>{props.children}</h1>
+  const Component = AHeaderTypeMap[props.as] || H1
+
+  return <Component className={clsx(props.className, classList)}>{props.children}</Component>
 }
 
 export default AHeading
