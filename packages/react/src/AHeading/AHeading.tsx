6b9c2cbe5781352f45b2141edf96d6745f2beb23
Christian Farris <christianfarriscs@gmail.com>
2021-02-16 19:54:11 -0600

    switch as to type, add functionality for as to choose styling.

--- packages/react/src/AHeading/AHeading.tsx
+++ packages/react/src/AHeading/AHeading.tsx
@@ -13,18 +13,27 @@ const AH3 = (props) => <h3 className={props.className}>{props.children}</h3>
 const AH4 = (props) => <h4 className={props.className}>{props.children}</h4>
 const AH5 = (props) => <h5 className={props.className}>{props.children}</h5>
 const AH6 = (props) => <h6 className={props.className}>{props.children}</h6>
-const ASubtitle1 = (props) => <strong className={`${props.className} a-subtitle-1`}>{props.children}</strong>
-const ASubtitle2 = (props) => <strong className={`${props.className} a-subtitle-2`}>{props.children}</strong>
+const ASubtitle1 = (props) => <strong className={props.className}>{props.children}</strong>
+const ASubtitle2 = (props) => <strong className={props.className}>{props.children}</strong>
 
 const AHeaderTypeMap = {
-  h1: AH1,
-  h2: AH2,
-  h3: AH3,
-  h4: AH4,
-  h5: AH5,
-  h6: AH6,
-  ['subtitle-1']: ASubtitle1,
-  ['subtitle-2']: ASubtitle2,
+  h1: 'a-h1',
+  h2: 'a-h2',
+  h3: 'a-h3',
+  h4: 'a-h4',
+  h5: 'a-h5',
+  h6: 'a-h6',
+  ['subtitle-1']: 'a-subtitle-1',
+  ['subtitle-2']: 'a-subtitle-2',
+}
+
+const getSubstitleClasses = (type) => {
+  const map = {
+    ['subtitle-1']: 'a-subtitle-1',
+    ['subtitle-2']: 'a-subtitle-2',
+  }
+
+  return map[type]
 }
 
 const AHeading: React.FC<AHeadingProps> = (props) => {
@@ -32,9 +41,11 @@ const AHeading: React.FC<AHeadingProps> = (props) => {
     'a-heading',
     isJustifiable(props.justify),
     isAlignable(props.align),
+    getSubstitleClasses(props.type),
+    AHeaderTypeMap[props.as],
   ]
 
-  const Component = AHeaderTypeMap[props.as] || AH1
+  const Component = AHeaderTypeMap[props.type] || AH1
 
   return <Component className={clsx(props.className, classList)}>{props.children}</Component>
 }
