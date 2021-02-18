53ece9c094c0203e876d581af590a96f76c24a7b
Christian Farris <christianfarriscs@gmail.com>
2021-02-17 18:55:02 -0600

    add ATextStyleMap

--- packages/react/src/AText/AText.tsx
+++ packages/react/src/AText/AText.tsx
@@ -10,6 +10,14 @@ import './AText.scss'
 const AP = (props) => <p className={props.className}>{props.children}</p>
 const AStrong = (props) => <strong className={props.className}>{props.children}</strong>
 
+const ATextStyleMap = {
+  'body-1': 'a-text--body-1',
+  'body-2': 'a-text--body-2',
+  'button': 'a-text--button',
+  'caption': 'a-text--caption',
+  'overline': 'a-text--overline',
+}
+
 const ATextTypeMap = {
   p: AP,
   strong: AStrong,
@@ -20,9 +28,10 @@ const AText: React.FC<ATextProps> = (props) => {
     'a-text',
     isJustifiable(props.justify),
     isAlignable(props.align),
+    ATextStyleMap[props.as],
   ]
 
-  const Component = ATextTypeMap[props.as] || AP
+  const Component = ATextTypeMap[props.type] || AP
 
   return <Component className={clsx(props.className, classList)}>{props.children}</Component>
 }
