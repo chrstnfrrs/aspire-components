6cfd472fbe5eada78618455816b17e760f33d1a5
Christian Farris <christianfarriscs@gmail.com>
2021-02-27 21:57:14 -0600

    change import order

--- packages/react/src/AContainer/AContainer.tsx
+++ packages/react/src/AContainer/AContainer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import clsx from 'clsx';
 
+import { AContainerProps } from './AContainer.types'
+import './AContainer.scss'
+
 import {isJustifiable, isAlignable} from 'aspire-components-helpers'
 import 'aspire-components-scss/build/index.scss'
 
-import { AContainerProps } from './AContainer.types'
-import './AContainer.scss'
 
 const AContainer: React.FC<AContainerProps> = (props) => {
   const classList = [
