43211827e5b63125d11c5282d9e6529b24eca538
Christian Farris <christianfarriscs@gmail.com>
2021-02-27 21:56:14 -0600

    add new componets

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,22 +1,28 @@
-import './index.scss'
 import ABtn from './ABtn/ABtn'
+import ABox from './ABox/ABox'
 import AContainer from './AContainer/AContainer'
 import AHeader from './AHeader/AHeader'
 import AHeading from './AHeading/AHeading'
 import AHero from './AHero/AHero'
 import AInput from './AInput/AInput'
+import ALink from './ALink/ALink'
 import ANav from './ANav/ANav'
 import ASection from './ASection/ASection'
 import AText from './AText/AText'
+import {setTheme} from './functions/theme'
+import './index.scss'
 
 export { 
   ABtn,
+  ABox,
   AContainer,
   AHeader,
   AHeading,
   AHero,
   AInput,
+  ALink,
   ANav,
   ASection,
   AText,
+  setTheme
 }
